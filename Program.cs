93e1ef9bb91fc1aa5050dfa6eca3e89648aefba6
Brian Martins2006 <brianmartins997232779@gmail.com>
2024-10-31 09:03:17 -0300

    primeiro

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Biblioteca_Matriz;
+using System.IO;
+
+
+class Program
+{
+    static void Main()
+    {
+        int linhas, colunas;
+        Console.WriteLine("**** Matrizes ****");
+        Console.WriteLine("Quantas linhas e colunas: ");
+        linhas = int.Parse(Console.ReadLine());
+        colunas = int.Parse(Console.ReadLine());
+        int[,] mat = new int[linhas, colunas];
+
+       // Console.WriteLine("Eentre com dados para a Matriz: ");
+        //Matriz.leia(mat);
+        Console.WriteLine("Dados da sua matriz: ");
+       // Matriz.mostra(mat);
+        Matriz.gera(mat);
+        Matriz.mostra(mat);
+        Console.ReadKey();
+    } 
+}
---
+++ lista_Matriz/Matriz.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+
+namespace Biblioteca_Matriz
+{
+    internal class Matriz
+    {
+        public static void leia(int[,] mat)
+        {
+            int linhas = mat.GetLength(0);
+            int coluna = mat.GetLength(1);
+
+            for (int i = 0; i < linhas; i++)
+            {
+                for (int j = 0; j < coluna; j++)
+                {
+                    Console.Write($"[{i},{j}]");
+                    mat[i, j] = int.Parse(Console.ReadLine());
+                }
+            }
+
+        }//fim do leia
+
+        public static void mostra(int[,] mat)
+        {
+            int linhas = mat.GetLength(0);
+            int coluna = mat.GetLength(1);
+
+            for (int i = 0; i < linhas; i++)
+            {
+                for (int j = 0; j < coluna; j++)
+                {
+                    Console.Write($"{mat[i, j],3} |");
+                }// fim for j
+
+                Console.WriteLine();
+            }//fim for i
+
+        }
+        public static void gera(int[,] mat)
+        {
+            int linha = mat.GetLength(0);
+            int coluna = mat.GetLength(1);
+
+                Random random = new Random();
+                for (int i = 0; i < linha; i++)
+                {
+                for(int j = 0;j < coluna; j++)
+                {
+                    mat[i, j] = random.Next(1, 100);
+                }
+
+                }
+
+            }//fim do gera
+        }
+    }
